0fcb37402034a86d95076d09d443700ed4f455ca
Amar Patel <theamarpatel@gmail.com>
2015-06-29 18:49:03 -0700

    initial commit

---
+++ calculator2/calculator2/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  calculator2
+//
+//  Created by Amar Patel on 6/29/15.
+//  Copyright (c) 2015 Amar Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    var brain = CalculatorBrain()
+    
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            display.text! += digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTyping = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTyping = false
+        }
+    }
+    
+}
+
